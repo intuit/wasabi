c079775e5b426a383495563341c5d88201b61338
Shaikh, Jamir <shaikhjamir@gmail.com>
2016-07-07 11:10:46 -0700

    added OS check for swagger

--- bin/build.sh
+++ bin/build.sh
@@ -134,9 +134,10 @@ if [ "${build}" = true ] && [ "$OS" == "OSX" ]; then
 fi
 
 content=${home}/${id}/content/ui/dist
-
-mkdir -p ${content}
-cp -R ./modules/ui/dist/ ${content}
-mkdir -p ${content}/swagger/swaggerjson
-cp -R ./modules/swagger-ui/target/swaggerui/ ${content}/swagger
-cp -R ./modules/api/target/generated/swagger-ui/swagger.json ${content}/swagger/swaggerjson
+if [ "$OS" == "OSX" ]; then
+  mkdir -p ${content}
+  cp -R ./modules/ui/dist/ ${content}
+  mkdir -p ${content}/swagger/swaggerjson
+  cp -R ./modules/swagger-ui/target/swaggerui/ ${content}/swagger
+  cp -R ./modules/api/target/generated/swagger-ui/swagger.json ${content}/swagger/swaggerjson
+fi
