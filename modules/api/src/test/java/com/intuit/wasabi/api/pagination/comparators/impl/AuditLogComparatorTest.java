bb7bb8412ddf1d6c5dbe1687da01cfaea025726b
Sebastian Höffner <info@sebastian-hoeffner.de>
2016-07-29 11:16:41 -0700

    AuditLogComparatorTest

--- modules/api/src/test/java/com/intuit/wasabi/api/pagination/comparators/impl/AuditLogComparatorTest.java
+++ modules/api/src/test/java/com/intuit/wasabi/api/pagination/comparators/impl/AuditLogComparatorTest.java
@@ -15,9 +15,106 @@
  *******************************************************************************/
 package com.intuit.wasabi.api.pagination.comparators.impl;
 
+import com.intuit.wasabi.auditlogobjects.AuditLogAction;
+import com.intuit.wasabi.auditlogobjects.AuditLogEntry;
+import com.intuit.wasabi.authenticationobjects.UserInfo;
+import com.intuit.wasabi.experimentobjects.Bucket;
+import com.intuit.wasabi.experimentobjects.Experiment;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
+
 /**
  * Tests for {@link AuditLogComparator}.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class AuditLogComparatorTest {
+    private AuditLogEntry auditLogEntry1;
+    private AuditLogEntry auditLogEntry2;
 
+    @Mock
+    private Experiment experiment;
+
+    @Before
+    public void setup() {
+        Experiment.ID experimentID = Experiment.ID.newInstance();
+        Mockito.doReturn(experimentID).when(experiment).getID();
+        Experiment.Label experimentLabel = Experiment.Label.valueOf("RedButtonExperience");
+        Mockito.doReturn(experimentLabel).when(experiment).getLabel();
+
+        auditLogEntry1 = new AuditLogEntry(
+                Calendar.getInstance(TimeZone.getTimeZone("GMT")),
+                UserInfo.from(UserInfo.Username.valueOf("john"))
+                        .withUserId("jodoe")
+                        .withEmail("jodoe@example.com")
+                        .withFirstName("John")
+                        .withLastName("Doe")
+                        .build(),
+                AuditLogAction.BUCKET_CHANGED,
+                experiment,
+                Bucket.Label.valueOf("A"),
+                "allocation",
+                "100",
+                "50");
+
+        Calendar secondDate = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        secondDate.add(Calendar.YEAR, 1);
+        auditLogEntry2 = new AuditLogEntry(
+                secondDate,
+                UserInfo.from(UserInfo.Username.valueOf("jane"))
+                        .withUserId("jadoe")
+                        .withEmail("jadoe@example.com")
+                        .withFirstName("Jane")
+                        .withLastName("Doe")
+                        .build(),
+                AuditLogAction.EXPERIMENT_CREATED,
+                experiment,
+                null,
+                "created",
+                null,
+                "Experiment new!");
+    }
+
+    @Test
+    public void testCompare() throws Exception {
+        AuditLogComparator auditLogComparator = new AuditLogComparator();
+
+        HashMap<String, Integer> testCases = new HashMap<>();
+        testCases.put("firstname", 1);
+        testCases.put("lastname", 0);
+        testCases.put("user", 1);
+        testCases.put("username", 1);
+        testCases.put("userid", 1);
+        testCases.put("mail", 1);
+        testCases.put("action", -1);
+        testCases.put("experiment", 0);
+        testCases.put("bucket", -1);
+        testCases.put("app", 0);
+        testCases.put("time", -1);
+        testCases.put("attr", -1);
+        testCases.put("before", -1);
+        testCases.put("after", -1);
+        testCases.put("description", -1);
+
+        for (Map.Entry<String, Integer> testCase : testCases.entrySet()) {
+            auditLogComparator.replaceSortorder(testCase.getKey());
+
+            int magnitudeResult = auditLogComparator.compare(auditLogEntry1, auditLogEntry2);
+            if (magnitudeResult != 0) {
+                magnitudeResult /= Math.abs(magnitudeResult);
+            }
+            Assert.assertEquals("test case " + testCase.getKey() + " failed.",
+                    testCase.getValue().intValue(),
+                    magnitudeResult);
+        }
+    }
 }
