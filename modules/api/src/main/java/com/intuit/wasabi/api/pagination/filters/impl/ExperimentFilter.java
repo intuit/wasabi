9b3b7436cee05e5a4b1759cb3032d282daae16b1
Sebastian Höffner <info@sebastian-hoeffner.de>
2016-07-30 12:01:25 -0700

    license formatting

--- modules/api/src/main/java/com/intuit/wasabi/api/pagination/filters/impl/ExperimentFilter.java
+++ modules/api/src/main/java/com/intuit/wasabi/api/pagination/filters/impl/ExperimentFilter.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2016 Intuit
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -134,7 +134,10 @@ public class ExperimentFilter extends PaginationFilter<Experiment> {
      * @param filter the filter value to check, see above for details.
      * @return true/false depending on the state check, see above for details.
      */
-    private static boolean stateTest(Experiment.State state, String filter) {
+    /*test*/ static boolean stateTest(Experiment.State state, String filter) {
+        if (state == Experiment.State.DELETED) {
+            return false;
+        }
         switch (filter.toLowerCase()) {
             case "notterminated":
                 return state == Experiment.State.DRAFT || state == Experiment.State.RUNNING || state == Experiment.State.PAUSED;
--- modules/api/src/test/java/com/intuit/wasabi/api/pagination/filters/impl/AuditLogFilterTest.java
+++ modules/api/src/test/java/com/intuit/wasabi/api/pagination/filters/impl/AuditLogFilterTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2016 Intuit
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,6 @@
  *******************************************************************************/
 package com.intuit.wasabi.api.pagination.filters.impl;
 
-import com.intuit.wasabi.api.pagination.comparators.impl.AuditLogComparator;
 import com.intuit.wasabi.auditlogobjects.AuditLogAction;
 import com.intuit.wasabi.auditlogobjects.AuditLogEntry;
 import com.intuit.wasabi.authenticationobjects.UserInfo;
