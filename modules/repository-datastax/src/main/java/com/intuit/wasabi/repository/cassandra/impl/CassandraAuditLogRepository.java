07c1e57e1f0ce0735352a5d8d2ed09f61ad7e9f6
mans4singh <mans4singh@yahoo.com>
2016-08-11 19:20:56 -0700

    Code cleanup and minor refactoring

--- modules/repository-datastax/src/main/java/com/intuit/wasabi/repository/cassandra/impl/CassandraAuditLogRepository.java
+++ modules/repository-datastax/src/main/java/com/intuit/wasabi/repository/cassandra/impl/CassandraAuditLogRepository.java
@@ -77,7 +77,7 @@ public class CassandraAuditLogRepository implements AuditLogRepository {
 		}
 	}
 
-	private String makeEmptyForNull(String input) {
+	private String makeEmptyStringForNull(String input) {
 		return input == null ? "" : input;
 	}
 
@@ -90,12 +90,12 @@ public class CassandraAuditLogRepository implements AuditLogRepository {
 			Calendar calendar = Calendar.getInstance();
 			calendar.setTime(auditLog.getTime());
 
-			String userName = makeEmptyForNull(auditLog.getUsername());
+			String userName = makeEmptyStringForNull(auditLog.getUsername());
 
-			String email = makeEmptyForNull(auditLog.getEmail());
-			String firstName = makeEmptyForNull(auditLog.getFirstName());
-			String lastName = makeEmptyForNull(auditLog.getLastName());
-			String userId = makeEmptyForNull(auditLog.getUserId());
+			String email = makeEmptyStringForNull(auditLog.getEmail());
+			String firstName = makeEmptyStringForNull(auditLog.getFirstName());
+			String lastName = makeEmptyStringForNull(auditLog.getLastName());
+			String userId = makeEmptyStringForNull(auditLog.getUserId());
 
 			UserInfo userInfo = UserInfo
 					.newInstance(Username.valueOf(userName)).withEmail(email)
@@ -297,26 +297,30 @@ public class CassandraAuditLogRepository implements AuditLogRepository {
 	 */
 	@Override
 	public boolean storeEntry(AuditLogEntry entry) {
+		// Note checks for action, time and action are redundant since 
+		// they are already checked in AuditLogEntry
 		if (entry == null || entry.getAction() == null
 				|| entry.getUser() == null || entry.getTime() == null) {
 			throw new RepositoryException("Can not insert AuditLogEntry "
 					+ entry + " into database, required values are null.");
 		}
 
+		// If application name is null use global entry name
 		String applicationName = AuditLogRepository.GLOBAL_ENTRY_APPLICATION
 				.toString();
 		if (entry.getApplicationName() != null
-				&& ! StringUtils.isBlank(entry.getApplicationName().toString()))
+				&& ! StringUtils.isBlank(entry.getApplicationName().toString())) {
 			applicationName = entry.getApplicationName().toString();
+		}
 
 		Date time = entry.getTime().getTime();
 		String action = entry.getAction().toString();
 
-		String firstName = makeEmptyForNull(entry.getUser().getFirstName());
-		String lastName = makeEmptyForNull(entry.getUser().getLastName());
-		String email = makeEmptyForNull(entry.getUser().getEmail());
-		String userId = makeEmptyForNull(entry.getUser().getUserId());
-		String userName = makeEmptyForNull(entry.getUser().getUsername()
+		String firstName = makeEmptyStringForNull(entry.getUser().getFirstName());
+		String lastName = makeEmptyStringForNull(entry.getUser().getLastName());
+		String email = makeEmptyStringForNull(entry.getUser().getEmail());
+		String userId = makeEmptyStringForNull(entry.getUser().getUserId());
+		String userName = makeEmptyStringForNull(entry.getUser().getUsername()
 				.getUsername());
 
 		UUID experimentId = entry.getExperimentId() == null ? null : entry
@@ -326,9 +330,9 @@ public class CassandraAuditLogRepository implements AuditLogRepository {
 		String bucketLabel = entry.getBucketLabel() == null ? "" : entry
 				.getBucketLabel().toString();
 
-		String changedProperty = makeEmptyForNull(entry.getChangedProperty());
-		String before = makeEmptyForNull(entry.getBefore());
-		String after = makeEmptyForNull(entry.getAfter());
+		String changedProperty = makeEmptyStringForNull(entry.getChangedProperty());
+		String before = makeEmptyStringForNull(entry.getBefore());
+		String after = makeEmptyStringForNull(entry.getAfter());
 
 		accessor.storeEntry(applicationName, time, action, firstName, lastName,
 				email, userName, userId, experimentId, experimentLabel,
@@ -337,184 +341,5 @@ public class CassandraAuditLogRepository implements AuditLogRepository {
 		return true;
 
 	}
-
-	/**
-	 * Allows cql select or update queries which have only an ApplicationName as
-	 * a prepared value.
-	 *
-	 * @param cql
-	 *            the query
-	 * @param applicationName
-	 *            the application to put in
-	 * @return the resulting rows.
-	 *
-	 * @throws RepositoryException
-	 *             if an {@link ConnectionException} occurs.
-	 */
-
-	/* test */
-	// Rows<Application.Name, String> cqlWithApplication(String cql,
-	// Application.Name applicationName) {
-	// try {
-	// OperationResult<CqlResult<Application.Name, String>> result =
-	// driver.getKeyspace()
-	// .prepareQuery(keyspace.auditlogCF())
-	// .withCql(cql)
-	// .asPreparedStatement()
-	// .withByteBufferValue(applicationName, ApplicationNameSerializer.get())
-	// .execute();
-	// return result.getResult().getRows();
-	// } catch (ConnectionException e) {
-	// throw new RepositoryException(
-	// String.format("Can not retrieve auditlog for application %s.",
-	// applicationName),
-	// e
-	// );
-	// }
-	// }
-
-	/**
-	 * Allows cql select or update queries which have no prepared values.
-	 *
-	 * @param cql
-	 *            the query
-	 * @return the resulting rows.
-	 *
-	 * @throws RepositoryException
-	 *             if an {@link ConnectionException} occurs.
-	 */
-	/* test */
-	// Rows<Application.Name, String> cqlSelectAll(String cql) {
-	// try {
-	// OperationResult<CqlResult<Application.Name, String>> result =
-	// driver.getKeyspace()
-	// .prepareQuery(keyspace.auditlogCF())
-	// .withCql(cql)
-	// .asPreparedStatement()
-	// .execute();
-	// return result.getResult().getRows();
-	// } catch (ConnectionException e) {
-	// throw new RepositoryException("Can not retrieve complete auditlog.", e);
-	// }
-	// }
-
-	/**
-	 * Reads {@link Rows} (for example from
-	 * {@link #cqlWithApplication(String, Application.Name)}) and returns the
-	 * contained list of audit log entries. <br />
-	 * If the rowKey is null, the key will be read from the query.
-	 *
-	 * @param rowKey
-	 *            the key used to query cassandra
-	 * @param rows
-	 *            the rows object
-	 * @return a list of AuditLogEntries
-	 */
-	/* test */
-	// List<AuditLogEntry> readAuditLogEntryList(Application.Name rowKey,
-	// Rows<Application.Name, String> rows) {
-	// List<AuditLogEntry> auditLogEntries = new ArrayList<>();
-	// for (Row<Application.Name, String> row : rows) {
-	// AuditLogEntry auditLogEntry;
-	// if ((auditLogEntry = readAuditLogEntry(rowKey == null ? row.getKey() :
-	// rowKey, row.getColumns())) != null) {
-	// auditLogEntries.add(auditLogEntry);
-	// }
-	// }
-	// return auditLogEntries;
-	// }
-
-	/**
-	 * Reads an AuditLogEntry from a ColumnList.
-	 *
-	 * @param applicationName
-	 *            the application name / row key
-	 * @param columnList
-	 *            the columnList
-	 * @return the auditLogEntry
-	 */
-	/* test */
-	// AuditLogEntry readAuditLogEntry(Application.Name applicationName,
-	// ColumnList<String> columnList) {
-	// if (columnList == null) {
-	// return null;
-	// }
-	// try {
-	// /// application name
-	// Application.Name appName =
-	// AuditLogRepository.GLOBAL_ENTRY_APPLICATION.equals(applicationName) ?
-	// null : applicationName;
-	//
-	// // deserialize time
-	// Calendar time = null;
-	// Date date;
-	// if ((date = columnList.getDateValue("time", null)) != null) {
-	// time = Calendar.getInstance();
-	// time.setTime(date);
-	// }
-	//
-	// // deserialize user
-	// String username = columnList.getStringValue("user_username", "");
-	// String firstname = columnList.getStringValue("user_firstname", "");
-	// String lastname = columnList.getStringValue("user_lastname", "");
-	// String email = columnList.getStringValue("user_email", "");
-	// String userid = columnList.getStringValue("user_userid", "");
-	// UserInfo user = null;
-	// if (!StringUtils.isBlank(username) || !StringUtils.isBlank(userid)) {
-	// user = (username != null ?
-	// UserInfo.from(UserInfo.Username.valueOf(username))
-	// : UserInfo.from(UserInfo.Username.valueOf(userid)))
-	// .withFirstName(StringUtils.isBlank(firstname) ? null : firstname)
-	// .withLastName(StringUtils.isBlank(lastname) ? null : lastname)
-	// .withUserId(StringUtils.isBlank(userid) ? null : userid)
-	// .withEmail(StringUtils.isBlank(email) ? null : email)
-	// .build();
-	// }
-	//
-	// // deserialize action
-	// String actionStr = columnList.getStringValue("action",
-	// AuditLogAction.UNSPECIFIED_ACTION.name());
-	// AuditLogAction action;
-	// //TODO: remove this try catch block.
-	// try {
-	// action = AuditLogAction.valueOf(actionStr);
-	// } catch (IllegalArgumentException e) {
-	// LOGGER.debug(String.format("Unknown action %s setting it to default.",
-	// actionStr), e);
-	// action = AuditLogAction.UNSPECIFIED_ACTION;
-	// }
-	//
-	// // deserialize experiment properties
-	// Experiment.Label experimentLabel =
-	// columnList.getValue("experiment_label", ExperimentLabelSerializer.get(),
-	// null);
-	// Experiment.ID experimentId = columnList.getValue("experiment_id",
-	// ExperimentIDSerializer.get(), null);
-	//
-	// // deserialize bucket properties
-	// Bucket.Label bucketLabel = columnList.getValue("bucket_label",
-	// BucketLabelSerializer.get(), null);
-	//
-	// // deserialize property changes
-	// String changedProperty = columnList.getStringValue("changed_property",
-	// "");
-	// changedProperty = StringUtils.isBlank(changedProperty) ? null :
-	// changedProperty;
-	// String propertyBefore = columnList.getStringValue("property_before", "");
-	// propertyBefore = StringUtils.isBlank(propertyBefore) ? null :
-	// propertyBefore;
-	// String propertyAfter = columnList.getStringValue("property_after", "");
-	// propertyAfter = StringUtils.isBlank(propertyAfter) ? null :
-	// propertyAfter;
-	//
-	// return new AuditLogEntry(time, user, action, appName,
-	// experimentLabel, experimentId, bucketLabel,
-	// changedProperty, propertyBefore, propertyAfter);
-	// } catch (IllegalArgumentException e) {
-	// LOGGER.warn("Can not read AuditLogEntry, data in auditlog column family might be corrupted for key '"
-	// + applicationName + "': ", e);
-	// }
-	// return null;
-	// }
 
 }
