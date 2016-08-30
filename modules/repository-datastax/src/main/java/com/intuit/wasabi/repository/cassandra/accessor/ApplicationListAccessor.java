43f9106b7312749a06baa0a1b91451fbad3068e4
mans4singh <mans4singh@yahoo.com>
2016-08-30 07:41:37 -0700

    Application list accessor + integration tests

--- modules/repository-datastax/src/main/java/com/intuit/wasabi/repository/cassandra/accessor/ApplicationListAccessor.java
+++ modules/repository-datastax/src/main/java/com/intuit/wasabi/repository/cassandra/accessor/ApplicationListAccessor.java
@@ -1,21 +1,51 @@
+/*******************************************************************************
+ * Copyright 2016 Intuit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package com.intuit.wasabi.repository.cassandra.accessor;
 
-import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.mapping.Result;
 import com.datastax.driver.mapping.annotations.Accessor;
 import com.datastax.driver.mapping.annotations.Query;
 import com.intuit.wasabi.repository.cassandra.pojo.ApplicationList;
 
+/**
+ * Application list accessor
+ *
+ */
 @Accessor
 public interface ApplicationListAccessor {
 
+	/**
+	 * Get application names
+	 * @return application names
+	 */
     @Query("select distinct app_name from applicationList")
     Result<ApplicationList> getUniqueAppName();
 
+    /**
+     * Delete application by name
+     * @param appName
+     */
     @Query("delete from applicationList where app_name = ?")
-    ResultSet deleteBy(String appName);
+    void deleteBy(String appName);
 
+    /**
+     * Insert application name into the table
+     * @param appName
+     */
     @Query("insert into applicationList (app_name) values (?)")
-    ResultSet insert(String appName);
+    void insert(String appName);
 
 }
