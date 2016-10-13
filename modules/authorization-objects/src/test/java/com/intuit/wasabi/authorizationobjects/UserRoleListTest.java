3d924d801e9e790f9b266e3f47c30337d59efaa1
Andrea <a.suckro@gmail.com>
2016-10-13 21:44:19 +0200

    updated unit tests for authorizationobjects

--- modules/authorization-objects/src/test/java/com/intuit/wasabi/authorizationobjects/UserRoleListTest.java
+++ modules/authorization-objects/src/test/java/com/intuit/wasabi/authorizationobjects/UserRoleListTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2016 Intuit
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserRoleListTest {
@@ -37,12 +36,6 @@ public class UserRoleListTest {
     @Before
     public void setUp() throws Exception {
         userRoleList = new UserRoleList();
-    }
-
-    @Test
-    public void testUserRoleList() {
-        assertNotNull(userRoleList.getRoleList());
-        assertNotNull(userRoleList.toString());
     }
 
     @Test
@@ -58,8 +51,7 @@ public class UserRoleListTest {
         UserRoleList newUserRoleList = new UserRoleList(5);
 
         assertEquals(userRoleList, newUserRoleList);
-        assertEquals(userRoleList, userRoleList);
-        assertEquals(userRoleList.hashCode(), userRoleList.hashCode());
+        assertEquals(userRoleList.hashCode(), newUserRoleList.hashCode());
     }
 
 
