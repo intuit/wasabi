791e7a1d17d7509413d921226ee34555639be045
jwtodd <james.w.todd@gmail.com>
2016-10-31 12:31:07 -0700

    mod: force deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
 #if [ "$TRAVIS_BRANCH" == 'develop' ] || [ "$TRAVIS_BRANCH" == 'master' ]; then
-  if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+#  if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy -DskipTests=true -P sign --settings .travis/settings.xml
-  fi
+#  fi
 #fi
