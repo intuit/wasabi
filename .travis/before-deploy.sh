2ad88449e8b6a4fe32970aff87b4d199d1e9900d
jwtodd <james.w.todd@gmail.com>
2016-10-31 13:40:19 -0700

    mod: revert .travis changes

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-#if [ "$TRAVIS_BRANCH" == 'develop' ] || [ "$TRAVIS_BRANCH" == 'master' ]; then
-#  if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" == 'develop' ] || [ "$TRAVIS_BRANCH" == 'master' ]; then
+  if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_68ab9ec6aea9_key -iv $encrypted_68ab9ec6aea9_iv \
       -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
     gpg --fast-import .travis/codesigning.asc
-#  fi
-#fi
+  fi
+fi
--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
-#if [ "$TRAVIS_BRANCH" == 'develop' ] || [ "$TRAVIS_BRANCH" == 'master' ]; then
-#  if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" == 'develop' ] || [ "$TRAVIS_BRANCH" == 'master' ]; then
+  if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy -DskipTests=true -P sign --settings .travis/settings.xml
-#  fi
-#fi
+  fi
+fi
