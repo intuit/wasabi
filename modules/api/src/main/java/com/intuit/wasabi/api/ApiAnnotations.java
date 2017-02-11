d4620a5a7022a2cab280c19eb3ea980012cb71ed
Abhishek Jain <abhi111jain@gmail.com>
2017-02-10 23:05:51 -0800

    Replaced string literals with constant variables in API module: Enables compile time checking of usages

---
+++ modules/api/src/main/java/com/intuit/wasabi/api/ApiAnnotations.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright 2016 Intuit
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.intuit.wasabi.api;
+
+public class ApiAnnotations {
+    public static final String APPLICATION_ID = "application.id";
+    public static final String DEFAULT_TIME_ZONE = "default.time.zone";
+    public static final String DEFAULT_TIME_FORMAT = "default.time.format";
+    public static final String ACCESS_CONTROL_MAX_AGE_DELTA_SECONDS = "access.control.max.age.delta.seconds";
+}
