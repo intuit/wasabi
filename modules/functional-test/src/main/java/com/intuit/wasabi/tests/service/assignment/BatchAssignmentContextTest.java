175aa7e73680ed019baec387c1d468331adddafe
Kethireddy <sudheer.kethireddy@yahoo.com>
2017-03-02 10:44:17 -0800

    formatted code as per wasabi standards

--- modules/functional-test/src/main/java/com/intuit/wasabi/tests/service/assignment/BatchAssignmentContextTest.java
+++ modules/functional-test/src/main/java/com/intuit/wasabi/tests/service/assignment/BatchAssignmentContextTest.java
@@ -1,17 +1,14 @@
 /*******************************************************************************
  * Copyright 2016 Intuit
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  *******************************************************************************/
 package com.intuit.wasabi.tests.service.assignment;
 
@@ -36,26 +33,22 @@ import com.intuit.wasabi.tests.model.factory.BucketFactory;
 import com.intuit.wasabi.tests.model.factory.ExperimentFactory;
 
 /**
- * Created on 03/01/2017
- * This test class tests scenario where the wasabi application
- * considers  batch-assignment calls with different contexts 
- * as two different assignments 
+ * Created on 03/01/2017 This test class tests scenario where the wasabi application considers batch-assignment calls
+ * with different contexts as two different assignments
  */
-public class BatchAssignmentContextTest extends TestBase{
+public class BatchAssignmentContextTest extends TestBase {
 
     Application batchAssignmentContextTestApp = null;
-    
-    List<Experiment> testExperimentsList ; // this holds experiments that are created in the test class to be deleted eventually
+
+    List<Experiment> testExperimentsList; // this holds experiments that are created in the test class to be deleted
+                                          // eventually
 
     int numberOfExperimentsPerBatch = 4; // number of experiments that are associated with a batch (page) call
 
-   
     @BeforeClass
-    public void setUp()
-    {
-        //lets create application and assign it to the variable
+    public void setUp() {
+        // lets create application and assign it to the variable
         batchAssignmentContextTestApp = ApplicationFactory.createApplication().setName("batchAssignmentContextTestApp");
-        
 
         // initialize the experiment list variable that holds list of all the experiments
         testExperimentsList = new ArrayList<Experiment>();
@@ -67,15 +60,16 @@ public class BatchAssignmentContextTest extends TestBase{
 
         List<Experiment> expList = new ArrayList<Experiment>();
 
-        for(int i = 1 ; i <= numberOfExperimentsPerBatch ; i++) {
-            Experiment exp = ExperimentFactory.createExperiment("_"+UUID.randomUUID().toString(),-1).setApplication(batchAssignmentContextTestApp);
+        for (int i = 1; i <= numberOfExperimentsPerBatch; i++) {
+            Experiment exp = ExperimentFactory.createExperiment("_" + UUID.randomUUID().toString(), -1)
+                    .setApplication(batchAssignmentContextTestApp);
             exp = postExperiment(exp);
 
-            //create buckets for the experiment
+            // create buckets for the experiment
             List<Bucket> bucketList = BucketFactory.createBuckets(exp, 5);
             postBuckets(bucketList);
 
-            //below we are starting the experiment
+            // below we are starting the experiment
             exp.state = Constants.EXPERIMENT_STATE_RUNNING;
             putExperiment(exp);
 
@@ -84,24 +78,26 @@ public class BatchAssignmentContextTest extends TestBase{
         }
 
         // do a POST of assignments for user1234 with context set as PROD
-        List<Assignment> assignmentsList = postAssignments(batchAssignmentContextTestApp, new User("user1234"), expList, "PROD");
+        List<Assignment> assignmentsList = postAssignments(batchAssignmentContextTestApp, new User("user1234"), expList,
+                "PROD");
 
-        //lets make sure that the response has   assignments equal to the number of experiments
+        // lets make sure that the response has assignments equal to the number of experiments
         Assert.assertEquals(assignmentsList.size(), numberOfExperimentsPerBatch);
 
-        //assert that all of the assignments are new for the user
-        for(Assignment assignment: assignmentsList) {
+        // assert that all of the assignments are new for the user
+        for (Assignment assignment : assignmentsList) {
             Assert.assertEquals(assignment.status, "NEW_ASSIGNMENT");
         }
 
-        // do a POST of assignments for user1234 with context set as DEV and this should all be NEW ASSIGNMENT as it is a different context
+        // do a POST of assignments for user1234 with context set as DEV and this should all be NEW ASSIGNMENT as it is
+        // a different context
         assignmentsList = postAssignments(batchAssignmentContextTestApp, new User("user1234"), expList, "DEV");
 
-        //lets make sure that the response has   assignments equal to the number of experiments
+        // lets make sure that the response has assignments equal to the number of experiments
         Assert.assertEquals(assignmentsList.size(), numberOfExperimentsPerBatch);
 
-        //assert that all of the assignments are new for the user
-        for(Assignment assignment: assignmentsList) {
+        // assert that all of the assignments are new for the user
+        for (Assignment assignment : assignmentsList) {
             Assert.assertEquals(assignment.status, "NEW_ASSIGNMENT");
         }
 
@@ -112,15 +108,16 @@ public class BatchAssignmentContextTest extends TestBase{
 
         List<Experiment> expList = new ArrayList<Experiment>();
 
-        for(int i = 1 ; i <= numberOfExperimentsPerBatch ; i++) {
-            Experiment exp = ExperimentFactory.createExperiment("_"+UUID.randomUUID().toString(),-5,-1).setApplication(batchAssignmentContextTestApp);
+        for (int i = 1; i <= numberOfExperimentsPerBatch; i++) {
+            Experiment exp = ExperimentFactory.createExperiment("_" + UUID.randomUUID().toString(), -5, -1)
+                    .setApplication(batchAssignmentContextTestApp);
             exp = postExperiment(exp);
 
-            //create buckets for the experiment
+            // create buckets for the experiment
             List<Bucket> bucketList = BucketFactory.createBuckets(exp, 5);
             postBuckets(bucketList);
 
-            //below we are starting the experiment
+            // below we are starting the experiment
             exp.state = Constants.EXPERIMENT_STATE_RUNNING;
             putExperiment(exp);
 
@@ -129,33 +126,34 @@ public class BatchAssignmentContextTest extends TestBase{
         }
 
         // do a POST of assignments for user5678 with context set as PROD
-        List<Assignment> assignmentsList = postAssignments(batchAssignmentContextTestApp, new User("user5678"), expList, "PROD");
+        List<Assignment> assignmentsList = postAssignments(batchAssignmentContextTestApp, new User("user5678"), expList,
+                "PROD");
 
-        //lets make sure that the response has   assignments equal to the number of experiments
+        // lets make sure that the response has assignments equal to the number of experiments
         Assert.assertEquals(assignmentsList.size(), numberOfExperimentsPerBatch);
 
-        //assert that all of the assignments are new for the user
-        for(Assignment assignment: assignmentsList) {
+        // assert that all of the assignments are new for the user
+        for (Assignment assignment : assignmentsList) {
             Assert.assertEquals(assignment.status, "EXPERIMENT_EXPIRED");
         }
 
-
     }
-    
+
     @Test
     public void createAssignmentForExperimentStartAndEndInFuture() {
 
         List<Experiment> expList = new ArrayList<Experiment>();
 
-        for(int i = 1 ; i <= numberOfExperimentsPerBatch ; i++) {
-            Experiment exp = ExperimentFactory.createExperiment("_"+UUID.randomUUID().toString(),1,5).setApplication(batchAssignmentContextTestApp);
+        for (int i = 1; i <= numberOfExperimentsPerBatch; i++) {
+            Experiment exp = ExperimentFactory.createExperiment("_" + UUID.randomUUID().toString(), 1, 5)
+                    .setApplication(batchAssignmentContextTestApp);
             exp = postExperiment(exp);
 
-            //create buckets for the experiment
+            // create buckets for the experiment
             List<Bucket> bucketList = BucketFactory.createBuckets(exp, 5);
             postBuckets(bucketList);
 
-            //below we are starting the experiment
+            // below we are starting the experiment
             exp.state = Constants.EXPERIMENT_STATE_RUNNING;
             putExperiment(exp);
 
@@ -164,37 +162,34 @@ public class BatchAssignmentContextTest extends TestBase{
         }
 
         // do a POST of assignments for user5678 with context set as PROD
-        List<Assignment> assignmentsList = postAssignments(batchAssignmentContextTestApp, new User("user5678"), expList, "PROD");
+        List<Assignment> assignmentsList = postAssignments(batchAssignmentContextTestApp, new User("user5678"), expList,
+                "PROD");
 
-        //lets make sure that the response has   assignments equal to the number of experiments
+        // lets make sure that the response has assignments equal to the number of experiments
         Assert.assertEquals(assignmentsList.size(), numberOfExperimentsPerBatch);
 
-        //assert that all of the assignments are new for the user
-        for(Assignment assignment: assignmentsList) {
+        // assert that all of the assignments are new for the user
+        for (Assignment assignment : assignmentsList) {
             Assert.assertEquals(assignment.status, "EXPERIMENT_NOT_STARTED");
         }
 
-
     }
-    
+
     /**
-     * The below method tears down the test experiments that are created
-     * that we stored using the testExperimentsList
+     * The below method tears down the test experiments that are created that we stored using the testExperimentsList
      */
     @AfterClass()
-    public void tearDown()
-    {
-        for(Experiment exp: testExperimentsList)
-        {
+    public void tearDown() {
+        for (Experiment exp : testExperimentsList) {
             exp.state = Constants.EXPERIMENT_STATE_PAUSED;
             putExperiment(exp);
 
-            //delete the experiment
+            // delete the experiment
             exp.state = Constants.EXPERIMENT_STATE_TERMINATED;
             putExperiment(exp);
         }
-        
-        //lets dereference the list and call the Garbage collector Daemon
+
+        // lets dereference the list and call the Garbage collector Daemon
         testExperimentsList = null;
         System.gc();
     }
--- modules/functional-test/src/main/java/com/intuit/wasabi/tests/service/assignment/SingleAssignmentContextTest.java
+++ modules/functional-test/src/main/java/com/intuit/wasabi/tests/service/assignment/SingleAssignmentContextTest.java
@@ -1,17 +1,14 @@
 /*******************************************************************************
  * Copyright 2016 Intuit
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  *******************************************************************************/
 package com.intuit.wasabi.tests.service.assignment;
 
@@ -37,153 +34,147 @@ import com.intuit.wasabi.tests.model.factory.BucketFactory;
 import com.intuit.wasabi.tests.model.factory.ExperimentFactory;
 
 /**
- * Created on 03/01/2017
- * This test class tests scenario where the wasabi application
- * considers a single-assignment calls with different contexts 
- * as two different assignments 
+ * Created on 03/01/2017 This test class tests scenario where the wasabi application considers a single-assignment calls
+ * with different contexts as two different assignments
  */
-public class SingleAssignmentContextTest extends TestBase{
+public class SingleAssignmentContextTest extends TestBase {
 
     Application singleAssignmentContextTestApp = null;
-    List<Experiment> testExperimentsList ;
+    List<Experiment> testExperimentsList;
 
     @BeforeClass
-    public void setUp()
-    {
-        //lets create application and assign it to the variable
-        singleAssignmentContextTestApp = ApplicationFactory.createApplication().setName("singleAssignmentContextTestApp");
+    public void setUp() {
+        // lets create application and assign it to the variable
+        singleAssignmentContextTestApp = ApplicationFactory.createApplication()
+                .setName("singleAssignmentContextTestApp");
 
         // initialize the experiment list variable that holds list of all the experiments
         testExperimentsList = new ArrayList<Experiment>();
+
+    }
+
+    /**
+     * This is the actual method that tests for SUT
+     */
+    @Test
+    public void createAssignmentForExperimentStartedInPast() {
         
+        // create experiment with start date as todaysdate -1 and assign it to the above created Application
+        Experiment exp = ExperimentFactory.createExperiment("_" + UUID.randomUUID().toString(), -1)
+                .setApplication(singleAssignmentContextTestApp);
+        exp = postExperiment(exp);
+
+        // add it to the list of test Experiments that will eventually be deleted
+        testExperimentsList.add(exp);
+
+        // create buckets for the experiment
+        List<Bucket> bucketList = BucketFactory.createBuckets(exp, 5);
+        postBuckets(bucketList);
+
+        // below we are starting the experiment
+        exp.state = Constants.EXPERIMENT_STATE_RUNNING;
+        putExperiment(exp);
+
+        // lets clear the cache and do an assignment for a user with context set to PROD
+        clearAssignmentsMetadataCache();
+        Assignment assignment = postAssignment(exp, new User("user123"), "PROD");
+
+        // lets assert the status and response code
+        assertReturnCode(response, HttpStatus.SC_OK);
+        Assert.assertEquals(assignment.status, "NEW_ASSIGNMENT");
+
+        // lets repeat the assignment for same user but with different context- context set to DEV
+        clearAssignmentsMetadataCache();
+        assignment = postAssignment(exp, new User("user123"), "DEV");
+
+        // lets assert the status and response code
+        assertReturnCode(response, HttpStatus.SC_OK);
+        Assert.assertEquals(assignment.status, "NEW_ASSIGNMENT");
+
+        // lets clear the cache and do an assignment for a user with context set to PROD so that we will get back
+        // existing assignment
+        clearAssignmentsMetadataCache();
+        assignment = postAssignment(exp, new User("user123"), "PROD");
+
+        // lets assert the status and response code
+        assertReturnCode(response, HttpStatus.SC_OK);
+        Assert.assertEquals(assignment.status, "EXISTING_ASSIGNMENT");
     }
 
     /**
-     * This is the actual method that tests
-     * for SUT
+     * This is the method that tests assignments for an experiment that has both start and end date in the past
      */
     @Test
-    public void createAssignmentForExperimentStartedInPast()
-    {
-        //create experiment with start date as todaysdate -1 and assign it to the above created Application
-        Experiment exp = ExperimentFactory.createExperiment("_"+UUID.randomUUID().toString(),-1).setApplication(singleAssignmentContextTestApp);
+    public void createAssignmentForExperimentStartedAndEndedInPast() {
+        // create experiment with start date and end date in the past and assign it to the above created Application
+        Experiment exp = ExperimentFactory.createExperiment("_" + UUID.randomUUID().toString(), -5, -1)
+                .setApplication(singleAssignmentContextTestApp);
         exp = postExperiment(exp);
 
-        //add it to the list of test Experiments that will eventually be deleted
+        // add it to the list of test Experiments that will eventually be deleted
         testExperimentsList.add(exp);
 
-        //create buckets for the experiment
+        // create buckets for the experiment
         List<Bucket> bucketList = BucketFactory.createBuckets(exp, 5);
         postBuckets(bucketList);
 
-        //below we are starting the experiment
+        // below we are starting the experiment
         exp.state = Constants.EXPERIMENT_STATE_RUNNING;
         putExperiment(exp);
 
-        //lets clear the cache and do an assignment for a user with context set to PROD
+        // lets clear the cache and do an assignment for a user with context set to PROD
         clearAssignmentsMetadataCache();
-        Assignment assignment = postAssignment(exp, new User("user123"),"PROD");
-
-        //lets assert the status and response code
+        Assignment assignment = postAssignment(exp, new User("user456"), "PROD");
+        System.out.println("assignment = " + assignment);
+        // lets assert the status and response code
         assertReturnCode(response, HttpStatus.SC_OK);
-        Assert.assertEquals(assignment.status,"NEW_ASSIGNMENT");
+        Assert.assertEquals(assignment.status, "EXPERIMENT_EXPIRED");
 
-        //lets repeat the assignment for same user but with different context- context set to DEV
-        clearAssignmentsMetadataCache();
-        assignment = postAssignment(exp, new User("user123"),"DEV");
-
-        //lets assert the status and response code
-        assertReturnCode(response, HttpStatus.SC_OK);
-        Assert.assertEquals(assignment.status,"NEW_ASSIGNMENT");
-
-        //lets clear the cache and do an assignment for a user with context set to PROD so that we will get back existing assignment
-        clearAssignmentsMetadataCache();
-        assignment = postAssignment(exp, new User("user123"),"PROD"); 
-
-        //lets assert the status and response code
-        assertReturnCode(response, HttpStatus.SC_OK);
-        Assert.assertEquals(assignment.status,"EXISTING_ASSIGNMENT");
     }
 
-    /**
-     * This is the  method that tests assignments for an experiment that has 
-     * both start and end date in the past
-     */
     @Test
-    public void createAssignmentForExperimentStartedAndEndedInPast()
-    {
-        //create experiment with start date and end date in the past and assign it to the above created Application
-        Experiment exp = ExperimentFactory.createExperiment("_"+UUID.randomUUID().toString(),-5,-1).setApplication(singleAssignmentContextTestApp);
+    public void createAssignmentForExperimentStartedAndEndedInFuture() {
+        // create experiment with start date and end date in the future and assign it to the above created Application
+        Experiment exp = ExperimentFactory.createExperiment("_" + UUID.randomUUID().toString(), 5, 10)
+                .setApplication(singleAssignmentContextTestApp);
         exp = postExperiment(exp);
 
-        //add it to the list of test Experiments that will eventually be deleted
+        // add it to the list of test Experiments that will eventually be deleted
         testExperimentsList.add(exp);
 
-        //create buckets for the experiment
+        // create buckets for the experiment
         List<Bucket> bucketList = BucketFactory.createBuckets(exp, 5);
         postBuckets(bucketList);
 
-        //below we are starting the experiment
+        // below we are starting the experiment
         exp.state = Constants.EXPERIMENT_STATE_RUNNING;
         putExperiment(exp);
 
-        //lets clear the cache and do an assignment for a user with context set to PROD
+        // lets clear the cache and do an assignment for a user with context set to PROD
         clearAssignmentsMetadataCache();
-        Assignment assignment = postAssignment(exp, new User("user456"),"PROD");
-        System.out.println("assignment = "+assignment);
-        //lets assert the status and response code
+        Assignment assignment = postAssignment(exp, new User("user456"), "PROD");
+
+        // lets assert the status and response code
         assertReturnCode(response, HttpStatus.SC_OK);
-        Assert.assertEquals(assignment.status,"EXPERIMENT_EXPIRED");
-
-    }
-    
-    @Test
-    public void createAssignmentForExperimentStartedAndEndedInFuture()
-    {
-        //create experiment with start date and end date in the future and assign it to the above created Application
-        Experiment exp = ExperimentFactory.createExperiment("_"+UUID.randomUUID().toString(),5,10).setApplication(singleAssignmentContextTestApp);
-        exp = postExperiment(exp);
-
-        //add it to the list of test Experiments that will eventually be deleted
-        testExperimentsList.add(exp);
-
-
-        //create buckets for the experiment
-        List<Bucket> bucketList = BucketFactory.createBuckets(exp, 5);
-        postBuckets(bucketList);
-
-        //below we are starting the experiment
-        exp.state = Constants.EXPERIMENT_STATE_RUNNING;
-        putExperiment(exp);
-
-        //lets clear the cache and do an assignment for a user with context set to PROD
-        clearAssignmentsMetadataCache();
-        Assignment assignment = postAssignment(exp, new User("user456"),"PROD");
-       
-        //lets assert the status and response code
-        assertReturnCode(response, HttpStatus.SC_OK);
-        Assert.assertEquals(assignment.status,"EXPERIMENT_NOT_STARTED");
+        Assert.assertEquals(assignment.status, "EXPERIMENT_NOT_STARTED");
 
     }
 
     /**
-     * The below method tears down the test experiments that are created
-     * that we stored using the testExperimentsList
+     * The below method tears down the test experiments that are created that we stored using the testExperimentsList
      */
     @AfterClass()
-    public void tearDown()
-    {
-        for(Experiment exp: testExperimentsList)
-        {
+    public void tearDown() {
+        for (Experiment exp : testExperimentsList) {
             exp.state = Constants.EXPERIMENT_STATE_PAUSED;
             putExperiment(exp);
 
-            //delete the experiment
+            // delete the experiment
             exp.state = Constants.EXPERIMENT_STATE_TERMINATED;
             putExperiment(exp);
         }
-        
-        //lets dereference the list and call the Garbage collector Daemon
+
+        // lets dereference the list and call the Garbage collector Daemon
         testExperimentsList = null;
         System.gc();
     }
