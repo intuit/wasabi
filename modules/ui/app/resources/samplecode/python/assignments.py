3641a23c7fa6801c2291a8c9edb9b1327992188c
Andrea <a.suckro@gmail.com>
2017-03-27 13:35:38 +0200

    Add assignments call in Python

---
+++ modules/ui/app/resources/samplecode/python/assignments.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+def get_assignment(application, experiment, user):
+    """
+    Get's an assignment for the given user and experiment. If no
+    assignment exists one will be generated.
+
+    Args:
+        application: the application the experiment runs in
+        experiment:  the running experiment for which the assignment should be retrieved
+        user:        the user who should be assigned
+    """
+    urlAssignment = "https://abtesting.intuit.com/api/v1/assignments/applications/%s/experiments/%s/users/%s" %(application, experiment, user);
+    r = requests.get(urlAssignment)
+    try:
+        assignment = json.loads(r.text)['assignment']
+    except KeyError as e:
+        if "EXPERIMENT_NOT_FOUND" in r.text:
+            print('The given Experiment is not found')
+        else:
+            raise
+
+if __name__ == "__main__":
+    application = 'ApplicationName'
+    experiment = 'ExperimentName'
+    user = 'UserName'
+
+    print(get_assignment(application, experiment, user))
