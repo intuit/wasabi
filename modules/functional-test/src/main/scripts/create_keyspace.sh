2cb196be3ebea52246e9dd103147a0f927dbebdf
jwu <jason_wu@intuit.com>
2017-05-08 15:52:40 -0700

    1. Updated create_keyspace to use #!/usr/bin/env bash 2. Updated maven-resources-plugin to use 3.0.0 3. Commented maven-filtering dependency in maven-resources-plugin in parent pom as it should be automatically figured out the right version.

--- modules/functional-test/src/main/scripts/create_keyspace.sh
+++ modules/functional-test/src/main/scripts/create_keyspace.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ###############################################################################
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 echo "Creating Keyspace ${CASSANDRA_KEYSPACE_PREFIX}_experiments if it not exits"
 echo "cqlsh " \
