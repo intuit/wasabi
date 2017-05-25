53cd253a5db6dda7451de9ac555fb56fe6c03b63
NileshB <barge.nilesh@gmail.com>
2017-05-25 12:16:19 -0700

    Consistency in installing JDK...

--- modules/functional-test/provision.sh
+++ modules/functional-test/provision.sh
@@ -26,7 +26,6 @@ sudo service httpd start
 
 # install Oracle JDK
 wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
-
 sudo yum -y localinstall jdk-8u131-linux-x64.rpm
 
 cat <<EOF | sudo tee -a /etc/yum.repos.d/datastax.repo
