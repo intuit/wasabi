7e8063abb46bc3620add846ef9a81257ac42e9a3
Abhishek Jain <abhi111jain@gmail.com>
2017-07-10 15:31:14 -0700

    Authentication enabled for user details API in authentication resource - corresponding to the hotfix

--- modules/api/src/test/java/com/intuit/wasabi/api/AuthenticationResourceTest.java
+++ modules/api/src/test/java/com/intuit/wasabi/api/AuthenticationResourceTest.java
@@ -18,6 +18,7 @@ package com.intuit.wasabi.api;
 import com.intuit.wasabi.authentication.Authentication;
 import com.intuit.wasabi.authenticationobjects.LoginToken;
 import com.intuit.wasabi.authenticationobjects.UserInfo;
+import com.intuit.wasabi.authorization.Authorization;
 import com.intuit.wasabi.exceptions.AuthenticationException;
 import org.junit.Before;
 import org.junit.Test;
@@ -42,9 +43,15 @@ public class AuthenticationResourceTest {
     @Mock
     Authentication authentication;
 
+    @Mock
+    Authorization authorization;
+
     @Before
     public void setup() {
-        authenticationResource = new AuthenticationResource(authentication, new HttpHeader("application-name", "600"));
+        authenticationResource =
+                new AuthenticationResource(authentication,
+                        new HttpHeader("application-name", "600"),
+                        authorization);
     }
 
     @Test
@@ -79,10 +86,11 @@ public class AuthenticationResourceTest {
 
     @Test
     public void getUserExists() throws Exception {
-
         UserInfo userInfo = UserInfo.newInstance(UserInfo.Username.valueOf("username")).build();
+        String authToken = "authorizationToken";
         when(authentication.getUserExists("username@a.b")).thenReturn(userInfo);
-        Response response = authenticationResource.getUserExists("username@a.b");
+        when(authorization.getUser(authToken)).thenReturn(null);
+        Response response = authenticationResource.getUserExists("username@a.b", authToken);
         assert (userInfo.equals(response.getEntity()));
     }
 }
