8808523a7f6adca12028ab31d8058e3e2a96e10f
Scott Cressler <longdogz@gmail.com>
2017-08-14 15:42:14 -0700

    Adding call to logout back in, as we need the backend to clear out HTTP-only cookies.

--- modules/ui/app/scripts/controllers/SignOutCtrl.js
+++ modules/ui/app/scripts/controllers/SignOutCtrl.js
@@ -4,19 +4,21 @@ angular.module('wasabi.controllers')
     .controller('SignOutCtrl', ['$scope', '$rootScope', '$state', 'AuthFactory', 'AUTH_EVENTS', 'Session', 'ConfigFactory',
             function ($scope, $rootScope, $state, AuthFactory, AUTH_EVENTS, Session, ConfigFactory) {
                 $scope.signOut = function () {
-                    if (ConfigFactory.authnType() === 'sso') {
+                    AuthFactory.signOut().$promise.then(function(/*result*/) {
                         Session.destroy();
-                        window.location.href = ConfigFactory.ssoLogoutRedirect();
-                    }
-                    else {
-                        AuthFactory.signOut().$promise.then(function(/*result*/) {
-                            Session.destroy();
+                        if (ConfigFactory.authnType() === 'sso') {
+                            window.location.href = ConfigFactory.ssoLogoutRedirect();
+                        }
+                        else {
                             $state.go('signin');
-                        }, function(/*reason*/) {
-                            $scope.loginFailed = true;
-                            $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
-                            $state.go('signin');
-                        });
-                    }
+                        }
+                    }, function(/*reason*/) {
+                        if (ConfigFactory.authnType() === 'sso') {
+                            window.location.href = ConfigFactory.ssoLogoutRedirect();
+                        }
+                        $scope.loginFailed = true;
+                        $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+                        $state.go('signin');
+                    });
                 };
             }]);
